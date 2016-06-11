1746651af8675fd865798bdc3fe477dc4f4fd371
Dinesh <dinesh.majeti@gmail.com>
2016-06-10 22:29:41 -0500

    push everything local to remote

--- R/pvalue.R
+++ R/pvalue.R
@@ -10,14 +10,14 @@
 pvalue <- function(x, y){
   testx <- shapiro.test(x)
   testy <- shapiro.test(y)
-  print(testx$p.value)
-  print(testy$p.value)
+  # print(testx$p.value)
+  # print(testy$p.value)
 
   if( testx$p.value > 0.05 & testy$p.value > 0.05) {
-    print("t-test")
+    # print("t-test")
     out <- t.test(x, y)
   } else {
-    print("wilcox test")
+    # print("wilcox test")
     out <- wilcox.test(x, y)
   }
 
